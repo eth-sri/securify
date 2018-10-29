4b916cef2ab8b68d90ed0cf94797b7ef7fa54071
ritzdorf <hubert@chainsecurity.com>
2018-10-29 08:52:34 +0100

    bringing back main decompiler

--- src/main/java/ch/securify/decompiler/instructions/StaticCall.java
+++ src/main/java/ch/securify/decompiler/instructions/StaticCall.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2018 Secure, Reliable, and Intelligent Systems Lab, ETH Zurich
+ *  Copyright 2018 ChainSecurity AG 
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
