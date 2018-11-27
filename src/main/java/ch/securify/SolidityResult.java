9b2a709866db1602887ac3b8f0ea8a4e20aa6980
Quentin Hibon <quentin@chainsecurity.com>
2018-11-27 13:45:07 +0100

    Revert "use class-level singleton"
    This reverts commit d7039d68cfc26fca029d496e259b18614886ad31.
    
    This commit introduced a bug leading to all the contracts returning the same
    results, since the instance was shared among analyzes.

--- src/main/java/ch/securify/SolidityResult.java
+++ src/main/java/ch/securify/SolidityResult.java
@@ -4,14 +4,7 @@ import java.util.TreeMap;
 import ch.securify.analysis.SecurifyErrors;
 
 public class SolidityResult {
-    private static final SolidityResult INSTANCE = new SolidityResult();
-    private SolidityResult(){}
-
-    public static SolidityResult getInstance(){
-        return INSTANCE;
-    }
-
     TreeMap<String, SmallPatternResult> results = new TreeMap<>();
 
-    SecurifyErrors securifyErrors = new SecurifyErrors();
+    SecurifyErrors securifyErrors = null;
 }
