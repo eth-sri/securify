aaa53b524300cc6bd865ad1ae89a81b9ace20a6e
Quentin Hibon <quentin@chainsecurity.com>
2018-11-28 10:25:45 +0100

    add surelyEndsBlock and endsExecution functions

--- src/main/java/ch/securify/decompiler/Destacker.java
+++ src/main/java/ch/securify/decompiler/Destacker.java
@@ -270,8 +270,7 @@ public class Destacker {
 				// continue on current branch
 				continue;
 			}
-			else if (opcode == OpCodes.STOP || opcode == OpCodes.RETURN || opcode == OpCodes.REVERT || opcode == OpCodes.SELFDESTRUCT || OpCodes.isInvalid(opcode)) {
-				// end of execution
+			else if (OpCodes.endsExecution(opcode)) {
 				return;
 			}
 			else if (opcode == OpCodes.JUMPDEST && pc != currentMethod && !canonicalStackForBranchJoinJumpdest.containsKey(pc)) {
