503d74fdfd383f8128dbb48a6fc47afd12b19830
Quentin Hibon <quentin@chainsecurity.com>
2019-01-21 18:25:01 +0100

    add checks when building souffle

--- build_souffle.sh
+++ build_souffle.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
+
+command -v souffle > /dev/null
+mkdir -p build
+pids=""
 for dl_file in smt_files/*.dl ; do
   output_file=build/$(basename -s '.dl' "$dl_file")
   if [ "$dl_file" -nt "$output_file" ]; then
-    souffle --dl-program="$output_file" "$dl_file" &
+    (souffle --dl-program="$output_file" "$dl_file") &
+    pids+="$! "
   fi
 done
 
-wait
+for pid in $pids ; do
+  wait $pid || exit 1
+done
