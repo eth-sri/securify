cb097c3bf61ebd3811295ad4100a494d3c5a6fdb
Quentin Hibon <quentin@chainsecurity.com>
2019-02-08 15:04:10 +0100

    make decompile test compatible with 0.5

--- src/test/resources/solidity/decompile.sol
+++ src/test/resources/solidity/decompile.sol
@@ -1,18 +1,18 @@
 contract A {
     uint a;
-    function f1(uint i) {
+    function f1(uint i) public {
         a = i;
     }
-    function f2(uint i) {
+    function f2(uint i) public {
         a = i;
     }
 
-    function g(uint i) {
-        var f = i < 100 ? f1 : f2;
+    function g(uint i) public {
+        function (uint256) f = i < 100 ? f1 : f2;
         f(i);
     }
 
-    function h() {
+    function h() public {
         msg.sender.transfer(a);
     }
 }
