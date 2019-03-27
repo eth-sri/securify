d419cd19d686dac35f4ece1812b9d7d0b7755d26
ritzdorf <hubert@chainsecurity.com>
2019-03-27 03:48:54 +0100

    Modify arithmetic shift

--- src/main/java/ch/securify/decompiler/instructions/Sar.java
+++ src/main/java/ch/securify/decompiler/instructions/Sar.java
@@ -16,9 +16,24 @@ public class Sar extends Instruction {
     public void computeResultValues() {
         if (getInput()[0].hasConstantValue() && getInput()[1].hasConstantValue() &&
                 getOutput()[0].getConstantValue() == Variable.VALUE_UNDEFINED) {
-            BigInteger a = BigIntUtil.fromInt256(getInput()[0].getConstantValue());
+        	BigInteger r;
+            BigInteger a = BigIntUtil.fromUint256(getInput()[0].getConstantValue());
             BigInteger b = BigIntUtil.fromInt256(getInput()[1].getConstantValue());
-            BigInteger r = b.shiftRight(a.intValueExact());
+            if(a.compareTo(BigInteger.valueOf(256)) >= 0) {
+            	if(b.signum() == -1) {
+            		r = BigInteger.valueOf(-1);
+            	}else {
+            		r = BigInteger.ZERO;
+            	}
+            } else if(b.signum() != -1) {
+                r = b.shiftRight(a.intValueExact());
+            } else  {
+            	r = b;
+            	for (int i = 0; i < a.intValueExact(); i++) {
+            		r.shiftRight(1);
+            		r.setBit(256);
+				}
+            }
             getOutput()[0].setConstantValue(BigIntUtil.toInt256(r));
         } else {
             getOutput()[0].setConstantValue(Variable.VALUE_ANY);
